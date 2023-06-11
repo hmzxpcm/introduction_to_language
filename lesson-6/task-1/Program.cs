696f29cac52c664e71aebe7f4154d8a3dc7f0ea9
hmzxp <hmzxp13@gmail.com>
2023-06-11 19:20:30 +0300

    make lesson 6

---
+++ lesson-6/task-1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] array1 = new int[] { 0, 7, 8, -2, -2 };
+int[] array2 = new int[] { 1, -7, 567, 89, 223 };
+
+
+int GetCountNumbersGreaterZeroFromArray(int[] array)
+{
+    int result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+
+    return result;
+}
+
+Console.WriteLine(GetCountNumbersGreaterZeroFromArray(array1));
+Console.WriteLine(GetCountNumbersGreaterZeroFromArray(array2));
---
+++ lesson-6/task-2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int[] firstLineData = new int[] { 2, 5 };
+int[] secondLineData = new int[] { 4, 9 };
+
+int[] GetIntersectionCoordinatesOfTwoLines(int[] firstLineData, int[] secondLineData)
+{
+    int m1 = firstLineData[0];
+    int b1 = firstLineData[1];
+    int m2 = secondLineData[0];
+    int b2 = secondLineData[1];
+
+    int x = (m2 - m1) / (b1 - b2);
+    int y = b1 * x + m1;
+
+    return new int[] { x, y };
+}
+
+void PrintCoordinates(int[] coordinates)
+{
+    Console.WriteLine("Точка пересечения: (" + coordinates[0] + "; " + coordinates[1] + ")");
+}
+
+PrintCoordinates(
+    GetIntersectionCoordinatesOfTwoLines(
+        firstLineData,
+        secondLineData
+    )
+);
